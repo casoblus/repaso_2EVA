bc7980e0d3f409c93675525fa2de5ea11171240f
casoblus <casobrinolusquinos@gmail.com>
2016-03-16 11:47:11 +0100

    clase Arrays

---
+++ Array.java
@@ -0,0 +1,50 @@
+/**
+ * Pruebas con la clase Array.
+ */
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+class TestArray
+{
+	private static void show( int[] array )
+	{
+		int len = array.length;
+		System.out.println( "CONTENIDO:" );
+		for ( int i = 0; i < len; i++ )
+		{
+			System.out.println( array[ i ] );
+		}
+		System.out.println( "===========================" );
+	}
+
+	public static void main ( String[] args )
+	{
+		// inicializo un array de enteros
+		
+		int[] enteros = new int[10];
+
+		// Relleno el array empleando el método fill
+
+		Arrays.fill ( enteros, 1 );
+		show( enteros );
+
+		// Cambio algunos valores
+		enteros[1] = 10;
+		enteros[3] = 123;
+		enteros[4] = 2;
+		enteros[6] = 34;
+		enteros[8] = 20;
+		enteros[9] = 11;
+		show( enteros );
+
+		// ordeno el array con sort
+		Arrays.sort( enteros );
+		show( enteros );
+
+		// busco 34 con binarySearch
+		System.out
+				.println( Arrays.binarySearch( enteros, 34 ) );
+
+	}
+}
---
+++ arrayList.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+class TestArrayList 
+{
+	private static void show( ArrayList<Integer> alst )
+	{
+			// tamaño del ArrayList
+			int len = alst.size();
+			
+			for ( int i = 0; i < len; i++ )
+			{
+				System.out
+						.println( alst.get(i) );
+			}
+	}
+	
+
+	public static void main ( String[] args )
+	{
+		// declaro arraylist
+		// ArrayList<int> enteros = new ArrayList<int>(); 
+		// ArrayList solo va con objetos
+		ArrayList<Integer> enteros = new ArrayList<Integer>();
+		
+		// introduzco valores con add
+		enteros.add(5);
+		enteros.add(18);
+		enteros.add(40);
+		enteros.add(12);
+		enteros.add(13);
+		enteros.add(34);
+	}
+}
+
